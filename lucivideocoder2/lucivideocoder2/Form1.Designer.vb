57a9a53f1de1e5d68f6d2855c484033505716d81
modernNeo <jsaadatm@sfu.ca>
2017-07-05 17:00:42 -0700

    added VB source code

---
+++ lucivideocoder2/lucivideocoder2/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.picFrame = New System.Windows.Forms.PictureBox()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.lblFileName = New System.Windows.Forms.Label()
+        Me.lblFrame = New System.Windows.Forms.Label()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.PicBoxEyesStates = New System.Windows.Forms.PictureBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtJumpToFrame = New System.Windows.Forms.TextBox()
+        Me.btnJumpToFrame = New System.Windows.Forms.Button()
+        Me.lblScaleForEyesClosed = New System.Windows.Forms.Label()
+        Me.numFldUpDownSelector = New System.Windows.Forms.NumericUpDown()
+        Me.chkBoxIDK = New System.Windows.Forms.CheckBox()
+        CType(Me.picFrame, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBoxEyesStates, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.numFldUpDownSelector, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picFrame
+        '
+        Me.picFrame.Location = New System.Drawing.Point(18, 18)
+        Me.picFrame.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.picFrame.Name = "picFrame"
+        Me.picFrame.Size = New System.Drawing.Size(960, 738)
+        Me.picFrame.TabIndex = 0
+        Me.picFrame.TabStop = False
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Location = New System.Drawing.Point(942, 775)
+        Me.btnOpenFile.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(36, 38)
+        Me.btnOpenFile.TabIndex = 1
+        Me.btnOpenFile.Text = "..."
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'lblFileName
+        '
+        Me.lblFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFileName.Location = New System.Drawing.Point(18, 775)
+        Me.lblFileName.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(914, 34)
+        Me.lblFileName.TabIndex = 2
+        Me.lblFileName.Text = "Folder Name ..."
+        Me.lblFileName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lblFrame
+        '
+        Me.lblFrame.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblFrame.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFrame.Location = New System.Drawing.Point(18, 869)
+        Me.lblFrame.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblFrame.Name = "lblFrame"
+        Me.lblFrame.Size = New System.Drawing.Size(370, 48)
+        Me.lblFrame.TabIndex = 3
+        Me.lblFrame.Text = "Frame: X, Time: Y"
+        Me.lblFrame.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PicBoxEyesStates
+        '
+        Me.PicBoxEyesStates.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PicBoxEyesStates.Image = Global.LuciVideoCoder2.My.Resources.Resources.EyeOpen
+        Me.PicBoxEyesStates.Location = New System.Drawing.Point(396, 828)
+        Me.PicBoxEyesStates.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.PicBoxEyesStates.Name = "PicBoxEyesStates"
+        Me.PicBoxEyesStates.Size = New System.Drawing.Size(90, 92)
+        Me.PicBoxEyesStates.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicBoxEyesStates.TabIndex = 4
+        Me.PicBoxEyesStates.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(825, 828)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(147, 94)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(670, 832)
+        Me.btnSave.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(147, 94)
+        Me.btnSave.TabIndex = 6
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 828)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(120, 20)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Jump to Frame:"
+        '
+        'txtJumpToFrame
+        '
+        Me.txtJumpToFrame.Location = New System.Drawing.Point(154, 828)
+        Me.txtJumpToFrame.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txtJumpToFrame.Name = "txtJumpToFrame"
+        Me.txtJumpToFrame.Size = New System.Drawing.Size(110, 26)
+        Me.txtJumpToFrame.TabIndex = 8
+        '
+        'btnJumpToFrame
+        '
+        Me.btnJumpToFrame.Location = New System.Drawing.Point(276, 829)
+        Me.btnJumpToFrame.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btnJumpToFrame.Name = "btnJumpToFrame"
+        Me.btnJumpToFrame.Size = New System.Drawing.Size(112, 35)
+        Me.btnJumpToFrame.TabIndex = 9
+        Me.btnJumpToFrame.Text = "Jump"
+        Me.btnJumpToFrame.UseVisualStyleBackColor = True
+        '
+        'lblScaleForEyesClosed
+        '
+        Me.lblScaleForEyesClosed.AutoSize = True
+        Me.lblScaleForEyesClosed.Location = New System.Drawing.Point(494, 831)
+        Me.lblScaleForEyesClosed.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lblScaleForEyesClosed.Name = "lblScaleForEyesClosed"
+        Me.lblScaleForEyesClosed.Size = New System.Drawing.Size(164, 20)
+        Me.lblScaleForEyesClosed.TabIndex = 11
+        Me.lblScaleForEyesClosed.Text = "Scale for Eyes Closed"
+        '
+        'numFldUpDownSelector
+        '
+        Me.numFldUpDownSelector.Increment = New Decimal(New Integer() {20, 0, 0, 0})
+        Me.numFldUpDownSelector.Location = New System.Drawing.Point(498, 856)
+        Me.numFldUpDownSelector.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.numFldUpDownSelector.Name = "numFldUpDownSelector"
+        Me.numFldUpDownSelector.Size = New System.Drawing.Size(98, 26)
+        Me.numFldUpDownSelector.TabIndex = 12
+        Me.numFldUpDownSelector.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'chkBoxIDK
+        '
+        Me.chkBoxIDK.AutoSize = True
+        Me.chkBoxIDK.Location = New System.Drawing.Point(498, 890)
+        Me.chkBoxIDK.Name = "chkBoxIDK"
+        Me.chkBoxIDK.Size = New System.Drawing.Size(120, 24)
+        Me.chkBoxIDK.TabIndex = 13
+        Me.chkBoxIDK.Text = "I don't know"
+        Me.chkBoxIDK.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(990, 940)
+        Me.Controls.Add(Me.chkBoxIDK)
+        Me.Controls.Add(Me.numFldUpDownSelector)
+        Me.Controls.Add(Me.lblScaleForEyesClosed)
+        Me.Controls.Add(Me.btnJumpToFrame)
+        Me.Controls.Add(Me.txtJumpToFrame)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.PicBoxEyesStates)
+        Me.Controls.Add(Me.lblFrame)
+        Me.Controls.Add(Me.lblFileName)
+        Me.Controls.Add(Me.btnOpenFile)
+        Me.Controls.Add(Me.picFrame)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picFrame, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBoxEyesStates, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.numFldUpDownSelector, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents picFrame As System.Windows.Forms.PictureBox
+    Friend WithEvents btnOpenFile As System.Windows.Forms.Button
+    Friend WithEvents lblFileName As System.Windows.Forms.Label
+    Friend WithEvents lblFrame As System.Windows.Forms.Label
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents PicBoxEyesStates As System.Windows.Forms.PictureBox
+   Friend WithEvents btnExit As System.Windows.Forms.Button
+   Friend WithEvents btnSave As System.Windows.Forms.Button
+   Friend WithEvents Label1 As System.Windows.Forms.Label
+   Friend WithEvents txtJumpToFrame As System.Windows.Forms.TextBox
+   Friend WithEvents btnJumpToFrame As System.Windows.Forms.Button
+    Friend WithEvents lblScaleForEyesClosed As Label
+    Friend WithEvents numFldUpDownSelector As NumericUpDown
+    Friend WithEvents chkBoxIDK As CheckBox
+End Class
---
+++ lucivideocoder2/lucivideocoder2/Form1.vb
@@ -0,0 +1,225 @@
+﻿Public Class Form1
+    Private EYESOPEN As Integer = 0
+    Private EYESCLOSED As Integer = 1
+    Private FPS = 11
+    Private CurrentFrame As Integer = 0
+    Private FrameCount As Integer = -1
+    Private EyeStatus As Integer = 0 ' 0=open, 1=closed
+    Private EyeScales As Integer = 100
+    Structure sample
+        Dim Frame As Integer
+        Dim time As Double
+        Dim eyestatus As Integer
+        Dim scaleForEyesClosed As Integer
+    End Structure
+    Private data(1) As sample
+
+    Private Sub btnOpenFile_Click(sender As System.Object, e As System.EventArgs) Handles btnOpenFile.Click
+
+        FolderBrowserDialog1.ShowDialog()
+        lblFileName.Text = FolderBrowserDialog1.SelectedPath
+        ' if the csv file exists, open it:
+        readdata()
+        ' pad currentframe with 0's:
+        LoadFrame()
+    End Sub
+
+    'Purpose: to read in the pictures from the selected folder
+    Sub readdata()
+        'Dim line As String
+        'If My.Computer.FileSystem.FileExists(lblFileName.Text & "\Output.csv") Then
+        '   Dim reader As New System.IO.StreamReader(lblFileName.Text & "\Output.csv")
+        '   While Not reader.EndOfStream
+        '      line = reader.ReadLine
+        '      ' parse line: (frame,time,eyeclosed)
+
+        '   End While
+        'End If
+        Dim numlines As Long
+        If My.Computer.FileSystem.FileExists(lblFileName.Text & "\Output.csv") Then
+            Using MyReader As New Microsoft.VisualBasic.FileIO.TextFieldParser(lblFileName.Text & "\Output.csv")
+                MyReader.TextFieldType = FileIO.FieldType.Delimited
+                MyReader.SetDelimiters(",")
+                Dim currentRow As String()
+                If Not MyReader.EndOfData Then
+                    currentRow = MyReader.ReadFields() ' skip the first row
+                End If
+                numlines = 0
+                FrameCount = 0
+                ReDim data(0)
+                While Not MyReader.EndOfData
+                    Try
+                        currentRow = MyReader.ReadFields()
+                        numlines = numlines + 1
+                        FrameCount = FrameCount + 1
+                        ReDim Preserve data(numlines)
+                        data(numlines).Frame = currentRow(0)
+                        data(numlines).time = currentRow(1)
+                        data(numlines).eyestatus = currentRow(2)
+                        data(numlines).scaleForEyesClosed = currentRow(3)
+                    Catch ex As Microsoft.VisualBasic.FileIO.MalformedLineException
+                        MsgBox("Line " & ex.Message & "is not valid and will be skipped.")
+                    End Try
+                End While
+            End Using
+        End If
+    End Sub
+
+    'To load pictures onto the Frame in the GUI
+    Private Sub LoadFrame()
+        Dim filename As String
+        Dim tmp As String
+        Dim framename As String
+        tmp = "000000"
+        framename = Trim$(CurrentFrame)
+        Mid(tmp, 7 - Len(framename), Len(framename)) = framename
+        filename = lblFileName.Text & "\" & tmp & ".jpg"
+        ' load first picture:
+        lblFrame.Text = "Frame: " & tmp & vbCrLf & "Time: " & CurrentFrame * 0.1757 ' 5.69 fps
+        Try
+            picFrame.Image = Image.FromFile(filename)
+        Catch
+            MsgBox("no more files in that direction; please scroll the other way")
+        End Try
+
+    End Sub
+
+    'Flips between pictures with the mouse wheel
+    Private Sub Form1_MouseWheel(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles Me.MouseWheel
+        If e.Delta > 0 Then
+            ' up
+            GoBackOne()
+        Else
+            ' down
+            GoForwardOne()
+        End If
+    End Sub
+
+
+
+    Private Sub GoBackOne()
+        If CurrentFrame > FrameCount Then
+            ' create new frame
+            ReDim Preserve data(CurrentFrame)
+            FrameCount = FrameCount + 1
+            ' eye status is what it was before
+        End If
+
+        ' save current frame
+        saveCurrentFrame()
+
+        If CurrentFrame = 0 Then
+            Exit Sub
+        End If
+        ' go back a frame
+        If CurrentFrame > 0 Then
+            CurrentFrame = CurrentFrame - 1
+            If data(CurrentFrame).eyestatus = EYESCLOSED Then
+                PicBoxEyesStates.Image = My.Resources.EyeClosed
+                EyeStatus = EYESCLOSED
+            Else
+                PicBoxEyesStates.Image = My.Resources.EyeOpen
+                EyeStatus = EYESOPEN
+            End If ' TODO: should this have an else???
+        End If
+        LoadFrame()
+    End Sub
+
+    Sub saveCurrentFrame()
+        With data(CurrentFrame)
+            If PicBoxEyesStates.Enabled = False Then
+                .eyestatus = 999
+            Else
+                .eyestatus = EyeStatus
+            End If
+
+            .Frame = CurrentFrame
+            .time = CurrentFrame * 0.1757
+            .scaleForEyesClosed = EyeScales
+        End With
+    End Sub
+
+    Private Sub GoForwardOne()
+        Dim created As Boolean
+        If CurrentFrame > FrameCount Then
+            ' create new frame
+            ReDim Preserve data(CurrentFrame)
+            FrameCount = FrameCount + 1
+            ' eye status is what it was before
+            created = True
+        Else
+            created = False
+        End If
+
+        ' save current frame
+        saveCurrentFrame()
+
+        ' go forward a frame
+        CurrentFrame = CurrentFrame + 1
+
+        ' load eye status
+        If CurrentFrame <= FrameCount Then
+            If data(CurrentFrame).eyestatus = EYESCLOSED Then
+                PicBoxEyesStates.Image = My.Resources.EyeClosed
+                EyeStatus = EYESCLOSED
+            ElseIf data(CurrentFrame).eyestatus = EYESOPEN Then
+                PicBoxEyesStates.Image = My.Resources.EyeOpen
+                EyeStatus = EYESOPEN
+            End If
+        End If
+        LoadFrame()
+    End Sub
+
+    'Switching the eye states pictures
+    Private Sub PictureBox1_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles PicBoxEyesStates.MouseUp
+        If EyeStatus = EYESOPEN Then
+            EyeStatus = EYESCLOSED
+            PicBoxEyesStates.Image = My.Resources.EyeClosed
+        Else
+            EyeStatus = EYESOPEN
+            PicBoxEyesStates.Image = My.Resources.EyeOpen
+        End If
+        btnExit.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Sub savedata()
+        Dim i As Integer
+        Dim tmpstr As String
+        tmpstr = "Frame,Time,EyeClosed, Scale For Eyes Closed" & vbCrLf
+        My.Computer.FileSystem.WriteAllText(lblFileName.Text & "\Output.csv", tmpstr, False)
+        For i = 1 To UBound(data)
+            tmpstr = Str$(data(i).Frame) & "," & Str$(data(i).time) & "," & Str$(data(i).eyestatus) & "," & Str$(data(i).scaleForEyesClosed) & vbCrLf
+            My.Computer.FileSystem.WriteAllText(FolderBrowserDialog1.SelectedPath & "\Output.csv", tmpstr, True)
+        Next
+    End Sub
+
+
+
+
+    Private Sub BtnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+        savedata()
+        btnExit.Enabled = True
+    End Sub
+
+    Private Sub btnJumpToFrame_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnJumpToFrame.Click
+        CurrentFrame = Val(txtJumpToFrame.Text) - 1
+        GoForwardOne()
+        LoadFrame()
+    End Sub
+
+    Private Sub chkBoxIDK_CheckedChanged(sender As Object, e As EventArgs) Handles chkBoxIDK.CheckedChanged
+        If (chkBoxIDK.Checked) Then
+            PicBoxEyesStates.Enabled = False
+        Else
+            PicBoxEyesStates.Enabled = True
+        End If
+    End Sub
+
+    Private Sub ScaleForEyesClosedChanged(sender As Object, e As EventArgs) Handles numFldUpDownSelector.ValueChanged
+        EyeScales = numFldUpDownSelector.Value
+    End Sub
+End Class
